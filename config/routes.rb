65d55b678735e0f9ada12ed460d446d2381d8f45
jjongppu <jjongppu@naver.com>
2025-08-12 11:01:38 +0900

    Rename plugin template to dk-market

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-MyPluginModule::Engine.routes.draw do
+DkMarket::Engine.routes.draw do
   get "/examples" => "examples#index"
   # define routes here
 end
 
-Discourse::Application.routes.draw { mount ::MyPluginModule::Engine, at: "my-plugin" }
+Discourse::Application.routes.draw { mount ::DkMarket::Engine, at: "dk-market" }
